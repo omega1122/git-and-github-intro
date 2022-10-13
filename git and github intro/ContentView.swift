fa26a0b1827ad25eb1fdd5ca84ae34630dc0582d
sus <amilucky8108@gmail.com>
2022-10-13 21:09:13 +0530

    Initial Commit

---
+++ git and github intro/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  git and github intro
+//
+//  Created by Ayan Rajpurohit on 13/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("GIT is here" )
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ git and github intro/git_and_github_introApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_and_github_introApp.swift
+//  git and github intro
+//
+//  Created by Ayan Rajpurohit on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct git_and_github_introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
